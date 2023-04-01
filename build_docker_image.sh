835c25c1b435b47877bd75eba05d9254ef45dc43
Hossein AKhlaghpour <hossein.akhlaghpour@intapp.com>
2023-04-01 16:28:09 +0000

    3 mai images

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,1 +1,10 @@
-docker build -f Dockerfile.python3.9-cuda12-runtime-base -t hossein20s/python3.9-cuda12-runtime:base
+
+# export docker_repo=docker.artifactory.dev.intapp.com/ai
+
+
+for TAG in base api ml
+do
+    docker build -f Dockerfile.python3.9-cuda12-runtime-$TAG -t python3.9-cuda12-runtime:$TAG .
+    docker tag python3.9-cuda12-runtime:$TAG "$docker_repo"/python3.9-cuda12-runtime:$TAG
+    docker push "$docker_repo"/python3.9-cuda12-runtime:$TAG
+done
