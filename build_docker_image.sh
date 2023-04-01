cfddf9fd962bb5d989a65bb0ba7e7118e0039cc9
Hossein AKhlaghpour <hossein.akhlaghpour@intapp.com>
2023-04-01 17:18:00 +0000

    placeholder

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -2,7 +2,7 @@
 # export docker_repo=docker.artifactory.dev.intapp.com/ai
 
 
-for TAG in base api ml non-root-ml
+for TAG in base api ml non-root-ml app
 do
     docker build -f Dockerfile.python3.9-cuda12-runtime-$TAG -t python3.9-cuda12-runtime:$TAG .
     docker tag python3.9-cuda12-runtime:$TAG "$docker_repo"/python3.9-cuda12-runtime:$TAG
